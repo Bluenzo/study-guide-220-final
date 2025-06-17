f90b39fdba788e8e86d5417f753e5bfcfa077b5f
Bluenzo <80460870+Bluenzo@users.noreply.github.com>
2025-06-17 08:45:16 -0700

    Updates study guide

--- src/FinalPractice.java
+++ src/FinalPractice.java
@@ -1,6 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
 public class FinalPractice {
     public static void main(String[] args) {
         // Feel free to call your methods here to experiment
+
+        ListNode head = new ListNode(4, new ListNode(8, new ListNode(4, new ListNode(16, new ListNode(8, new ListNode(42, new ListNode(11, new ListNode(29, new ListNode(42, new ListNode(34))))))))));
+
+        int oddIndexSumResult = oddIndexSum(head);
+        System.out.println("Sum of values at odd indexes: " + oddIndexSumResult);
+
+        int numberValuesResult = numberValuesShowsUp(head);
+        System.out.println("Number of unique values in the linked list: " + numberValuesResult);
+
+        TreeNode root = new TreeNode(10);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(12);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(1);
+        root.right.right = new TreeNode(8);
+        root.left.left.left = new TreeNode(2);
+        root.left.left.right = new TreeNode(9);
+        root.right.right.left = new TreeNode(11);
+        root.right.right.right = new TreeNode(13);
+        root.right.right.left.left = new TreeNode(18);
+
+        int result = leafNodesEvenValue(root);
+        System.out.println("Sum of even-valued leaf nodes: " + result);
+
+        int result1 = sumOfBranchNodesWithOddValue(root);
+        System.out.println("Sum of odd-valued branch nodes: " + result1);
+
 
         // YOU MUST ALSO MAKE THOROUGH TESTS FOR EVERY METHOD 
     }
@@ -17,8 +46,109 @@ public class FinalPractice {
      */
     public static int oddIndexSum(ListNode head) {
         // TODO: implement this AND MAKE MORE UNIT TESTS FOR IT
-        return -1;
+        if(head == null) {
+            return 0;
+        }
+
+        int sum = 0;
+
+        ListNode current = head;
+        int index = 0;
+        while (current != null){
+            if(index % 2 != 0){
+                sum += current.data;
+            }
+            current = current.next;
+            index++;
+        }
+        return sum;
     }
 
     // TODO: implement the rest of the study guide AND MAKE GOOD UNIT TESTS
+
+    public static int largestOfThreeLastValue(ListNode head) {
+        if(head == null) {
+            return Integer.MIN_VALUE; 
+        }
+
+        int largest = Integer.MIN_VALUE;
+        ListNode current = head;
+        while(current != null){
+
+
+        }
+    }
+
+    public static int leafNodesEvenValue (TreeNode node){
+        if(node == null){
+            return 0;
+        }
+        int sum = 0;
+        if(node.left == null && node.right == null){
+            if(node.data % 2 ==0){
+                sum += node.data;
+            }
+        
+        }
+        return sum + leafNodesEvenValue(node.left) + leafNodesEvenValue(node.right);
+
+    }
+
+    public static int sumOfBranchNodesWithOddValue(TreeNode node){
+        if(node == null){
+            return 0;
+        }
+
+        int sum = 0;
+
+        if(node.left != null || node.right != null){
+            if(node.data % 2 != 0){
+                sum += node.data;
+            }
+        }
+        
+        return sum + sumOfBranchNodesWithOddValue(node.left) + sumOfBranchNodesWithOddValue(node.right);
+    }
+
+    /**
+     * Given a reference to a head node in a linked list of integers, return a Map<Integer, Integer> containing each value along with the number of times it shows up.
+    Example:
+    Linked List:
+    4 -> 34 -> 4 -> 16 -> 4 -> 16 -> 29 -> 8 -> 8
+
+    Expected Return Map (order is unimportant): 
+    { 
+    4: 3,
+    8: 2,
+    16: 2,
+    29: 1,
+    34: 1
+    } 
+
+     * @param head
+     * @return
+     */
+    
+
+
+    public static int numberValuesShowsUp(ListNode head){
+        if(head == null){
+            return 0;
+        }
+        Map<Integer, Integer> valueCountMap = new HashMap<>();
+
+        ListNode current = head;
+        while(current != null){
+            int value = current.data;
+            valueCountMap.put(value, valueCountMap.getOrDefault(value, 0) + 1);
+            current = current.next;
+        }
+        // Print the map for demonstration purposes
+        for(Map.Entry<Integer, Integer> entry : valueCountMap.entrySet()){
+            System.out.println("Value: " + entry.getKey() + ", Count: " + entry.getValue());
+        }
+        return valueCountMap.size(); 
+    }
+
+
 }
--- src/FinalPracticeTest.java
+++ src/FinalPracticeTest.java
@@ -10,6 +10,29 @@ public class FinalPracticeTest {
         assertEquals(95, actual);
     }
 
+    @Test
+    void testOddIndexSum_singleNodeList() {
+        ListNode list = new ListNode(10);
+        int actual = FinalPractice.oddIndexSum(list);
+        assertEquals(0, actual); // Index 0 is even
+    }
+
+    @Test
+    void testOddIndexSum_twoNodeList() {
+        ListNode list = new ListNode(5, new ListNode(15));
+        int actual = FinalPractice.oddIndexSum(list);
+        assertEquals(15, actual); // 15 is at index 1
+    }
+
+    @Test
+    void testOddIndexSum_evenLengthList() {
+        ListNode list = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
+        int actual = FinalPractice.oddIndexSum(list);
+        assertEquals(6, actual);
+    }
+    
+
+
     // TODO: Make more tests for oddIndexSum
 
     // TODO: Make thorough tests for ALL the questions on the study guide
